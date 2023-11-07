e467ce803022013c0b7c500a3dec2e254aac669a
Ibbocs <99998326+Ibbocs@users.noreply.github.com>
2023-11-07 15:59:08 +0400

    Add project files.

---
+++ HttpClient/Controllers/JsonPlaceholdeController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace HttpClient.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JsonPlaceholdeController : ControllerBase
+    {
+        //private HttpClient client
+        //private readonly IHttpClientFactory _httpClientFactory;
+
+        private string baseUrl = "https://jsonplaceholder.typicode.com/";
+        //private System.Net.Http.HttpClient _client;
+
+        //public JsonPlaceholdeController(System.Net.Http.HttpClient client)
+        //{
+        //    _client = client;
+        //    client.BaseAddress = new Uri(baseUrl);
+        //}
+
+
+        [HttpGet]
+        public async Task<List<Todo>> Get()
+        {
+            //var client = _httpClientFactory.CreateClient();
+
+            using (System.Net.Http.HttpClient client = new System.Net.Http.HttpClient())
+            {
+                try
+                {
+                    client.BaseAddress = new Uri(baseUrl);
+
+                    //var response = await client.GetStringAsync("todos");
+                    HttpResponseMessage response = await client.GetAsync("todos");
+
+                    string data = await response.Content.ReadAsStringAsync();
+
+                    //var model = JsonConvert.DeserializeObject<List<Todo>>(data);
+
+                    List<Todo>? todoList = await response.Content.ReadFromJsonAsync<List<Todo>>();
+
+                    return todoList;
+                }
+                catch (Exception ex)
+                {
+                    await Console.Out.WriteLineAsync(ex.Message + ex.InnerException);
+                    return null;
+                }
+                finally
+                {
+                    client.Dispose();
+                }
+
+            }
+
+        }
+
+        [HttpPost]
+        public async Task<CreatePost> Post([FromBody] CreatePostDTO model)
+        {
+
+            using System.Net.Http.HttpClient client = new System.Net.Http.HttpClient();
+            client.BaseAddress= new Uri(baseUrl);
+
+            //var contentModel = JsonConvert.SerializeObject(model);         //bunu yazmayanda data null gedir
+            //var myContent = new StringContent(contentModel, Encoding.UTF8, "application/json");
+            //var res = await client.PostAsync("posts", myContent);
+            //var dataPostAsync = await res.Content.ReadFromJsonAsync<CreatePost>();
+            //return dataPostAsync;
+
+            HttpResponseMessage response = await client.PostAsJsonAsync("posts", model);
+            var dataPostAsJsonAsync = await response.Content.ReadFromJsonAsync<CreatePost>();
+
+            return dataPostAsJsonAsync;
+        }
+    }
+}
---
+++ HttpClient/Todo.cs
@@ -0,0 +1,26 @@
+﻿namespace HttpClient
+{
+    public class Todo
+    {
+        public int UserId { get; set; }
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool Completed { get; set; }
+    }
+
+    public class CreatePost
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public int UserId { get; set; }
+    }
+
+    public class CreatePostDTO
+    {
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public int UserId { get; set; }
+    }
+
+}
